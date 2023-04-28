76f6590876378986b7629899a6a7c40547ad63f5
Daniil Trishkin <kernel.pryanic@protonmail.com>
2023-04-28 23:43:54 +0200

    Complete unit tests and make small improvements

--- goudpscan/scanner_test.go
+++ goudpscan/scanner_test.go
@@ -1,11 +1,34 @@
 package goudpscan_test
 
 import (
-	"reflect"
+	"errors"
+	"log"
+	"net"
+	"os"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/KernelPryanic/goudpscan/goudpscan"
+	"github.com/stretchr/testify/assert"
 )
+
+func TestNewOptions(t *testing.T) {
+	options := goudpscan.NewOptions(true, uint(10), uint8(3), 2)
+
+	assert.NotNil(t, options, "NewOptions should not return nil")
+}
+
+func TestNewScanner(t *testing.T) {
+	scanner := goudpscan.New(
+		[]string{"127.0.0.1"},
+		[]string{"80"},
+		map[uint16][]string{},
+		goudpscan.NewOptions(true, uint(10), uint8(3), 2),
+	)
+
+	assert.NotNil(t, scanner, "New should not return nil")
+}
 
 func TestSegmentation(t *testing.T) {
 	tests := []struct {
@@ -28,17 +51,33 @@ func TestSegmentation(t *testing.T) {
 
 func TestBreakUpIP(t *testing.T) {
 	tests := []struct {
-		segments []string
-		expected []string
+		segments    []string
+		expected    []string
+		expectedErr bool
 	}{
-		{[]string{"192", "168", "0", "1"}, []string{"192.168.0.1"}},
-		{[]string{"10", "0", "0", "1-3"}, []string{"10.0.0.1", "10.0.0.2", "10.0.0.3"}},
-		{[]string{"10", "0-1", "0", "1-3"}, []string{"10.0.0.1", "10.0.0.2", "10.0.0.3", "10.1.0.1", "10.1.0.2", "10.1.0.3"}},
+		{segments: []string{"192", "168", "0", "1"}, expected: []string{"192.168.0.1"}},
+		{segments: []string{"10", "0", "0", "1-3"}, expected: []string{"10.0.0.1", "10.0.0.2", "10.0.0.3"}},
+		{
+			segments: []string{"10", "0-1", "0", "1-3"},
+			expected: []string{"10.0.0.1", "10.0.0.2", "10.0.0.3", "10.1.0.1", "10.1.0.2", "10.1.0.3"},
+		},
+		{
+			segments:    []string{"10", "0,1-2", "0", "24"},
+			expectedErr: true,
+		},
+		{
+			segments:    []string{"10", "0-1,1", "0", "24"},
+			expectedErr: true,
+		},
+		{
+			segments:    []string{"10", "0-1", "0,1-1", "24"},
+			expectedErr: true,
+		},
 	}
 
 	for _, test := range tests {
 		ips, err := goudpscan.BreakUpIP(test.segments)
-		if err != nil {
+		if (err != nil) != test.expectedErr {
 			t.Errorf("BreakUpIP(%v) returned an error: %v", test.segments, err)
 			continue
 		}
@@ -50,18 +89,21 @@ func TestBreakUpIP(t *testing.T) {
 
 func TestParseSubnet(t *testing.T) {
 	tests := []struct {
-		subnet   string
-		expected []string
+		subnet      string
+		expected    []string
+		expectedErr bool
 	}{
-		{"192.168.0.1", []string{"192.168.0.1"}},
-		{"10.0.0.0/24", []string{"10.0.0.0/24"}},
-		{"10.0.0.1-4", []string{"10.0.0.1", "10.0.0.2", "10.0.0.3", "10.0.0.4"}},
-		{"10.0.0.1-2/24", []string{"10.0.0.1/24", "10.0.0.2/24"}},
+		{subnet: "192.168.0.1", expected: []string{"192.168.0.1"}},
+		{subnet: "10.0.0.0/24", expected: []string{"10.0.0.0/24"}},
+		{subnet: "10.0.0.1-4", expected: []string{"10.0.0.1", "10.0.0.2", "10.0.0.3", "10.0.0.4"}},
+		{subnet: "10.0.0.1-2/24", expected: []string{"10.0.0.1/24", "10.0.0.2/24"}},
+		{subnet: "10.0,1-2.1-2/24", expectedErr: true},
+		{subnet: "10.0,1-2.1-2", expectedErr: true},
 	}
 
 	for _, test := range tests {
 		subnets, err := goudpscan.ParseSubnet(test.subnet)
-		if err != nil {
+		if (err != nil) != test.expectedErr {
 			t.Errorf("ParseSubnet(%q) returned an error: %v", test.subnet, err)
 			continue
 		}
@@ -73,16 +115,21 @@ func TestParseSubnet(t *testing.T) {
 
 func TestBreakUPPort(t *testing.T) {
 	tests := []struct {
-		portRange string
-		expected  []uint16
+		portRange   string
+		expected    []uint16
+		expectedErr bool
 	}{
-		{"80", []uint16{80}},
-		{"22-25", []uint16{22, 23, 24, 25}},
+		{portRange: "80", expected: []uint16{80}},
+		{portRange: "22-25", expected: []uint16{22, 23, 24, 25}},
+		{portRange: "22-25,80", expected: []uint16{22, 23, 24, 25, 80}},
+		{portRange: "80.0", expectedErr: true},
+		{portRange: "22.2-25", expectedErr: true},
+		{portRange: "22-25.5", expectedErr: true},
 	}
 
 	for _, test := range tests {
 		ports, err := goudpscan.BreakUPPort(test.portRange)
-		if err != nil {
+		if (err != nil) != test.expectedErr {
 			t.Errorf("BreakUPPort(%q) returned an error: %v", test.portRange, err)
 			continue
 		}
@@ -100,16 +147,14 @@ func TestHosts(t *testing.T) {
 		expectedErr bool
 	}{
 		{
-			name:        "Single IP",
-			cidr:        "192.168.1.1",
-			expected:    []string{"192.168.1.1"},
-			expectedErr: false,
+			name:     "Single IP",
+			cidr:     "192.168.1.1",
+			expected: []string{"192.168.1.1"},
 		},
 		{
-			name:        "CIDR /30",
-			cidr:        "192.168.1.0/30",
-			expected:    []string{"192.168.1.0", "192.168.1.1", "192.168.1.2", "192.168.1.3"},
-			expectedErr: false,
+			name:     "CIDR /30",
+			cidr:     "192.168.1.0/30",
+			expected: []string{"192.168.1.0", "192.168.1.1", "192.168.1.2", "192.168.1.3"},
 		},
 		{
 			name:        "Invalid CIDR",
@@ -118,18 +163,58 @@ func TestHosts(t *testing.T) {
 		},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := goudpscan.Hosts(tt.cidr)
-			if (err != nil) != tt.expectedErr {
-				t.Errorf("Hosts() error = %v, expectedErr %v", err, tt.expectedErr)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := goudpscan.Hosts(test.cidr)
+			if (err != nil) != test.expectedErr {
+				t.Errorf("Hosts() error = %v, expectedErr %v", err, test.expectedErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.expected) {
-				t.Errorf("Hosts() = %v, expected %v", got, tt.expected)
+			if !equal(got, test.expected) {
+				t.Errorf("Hosts() = %v, expected %v", got, test.expected)
 			}
 		})
 	}
+}
+
+func TestScan(t *testing.T) {
+	hosts := []string{"127.0.0.1"}
+	ports := []string{"80"}
+	payloads := make(map[uint16][]string)
+	opts := goudpscan.NewOptions(true, 1, 0, 1)
+
+	sc := goudpscan.New(hosts, ports, payloads, opts)
+	errLog := log.New(os.Stderr, "", 0)
+
+	// Create a channel to stop the SniffICMP function
+	ch := make(chan bool)
+
+	// Run the SniffICMP function in a separate goroutine
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		if err := goudpscan.SniffICMP(ch, &wg); err != nil && !errors.Is(err, net.ErrClosed) {
+			t.Errorf("SniffICMP failed: %v", err)
+		}
+	}()
+
+	time.Sleep(1 * time.Second)
+
+	// Run the Scan function
+	scanResult, err := sc.Scan(errLog, ch)
+	if err != nil {
+		t.Errorf("Scan failed: %v", err)
+	}
+
+	// Check the result of the scan
+	expectedKey := "127.0.0.1:80"
+	if _, ok := scanResult[expectedKey]; !ok {
+		t.Errorf("Scan result does not contain the expected key: %s", expectedKey)
+	}
+
+	// Stop the SniffICMP function
+	ch <- true
+	wg.Wait()
 }
 
 // Helper function to compare two string slices
